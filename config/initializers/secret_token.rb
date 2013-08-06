f34096f931d15e14bfce1dcfe56e54b7c8bb541f
shielasalamat <ssalamat@appsource.biz>
2013-08-06 17:53:19 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ZurbFoundationTest::Application.config.secret_key_base = 'e82941ade97bb07e685c12ac1fea2afe436c74846db7a49e880ab3ce183c2660df4900e7d0495e13b015cdc41cb4d6fbbb7f5532301e17c00575402f5c0617cf'
